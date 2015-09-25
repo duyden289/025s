1da0cfc0ebe3cad6698237883853e0fdd7504ff3
Nguyen Huu Den <nguyenhuuden289@gmail.com>
2015-09-25 11:17:44 +0700

    Perform check timer and check result

--- Maths025s/Math025s/HDCalculateViewController.m
+++ Maths025s/Math025s/HDCalculateViewController.m
@@ -21,6 +21,12 @@
 @property (weak, nonatomic) IBOutlet UILabel *numberOne;
 @property (weak, nonatomic) IBOutlet UILabel *numberTwo;
 @property (weak, nonatomic) IBOutlet UILabel *resultCalculate;
+@property (weak, nonatomic) IBOutlet UILabel *labelTime;
+
+@property (nonatomic, strong) NSTimer *timer;
+@property (nonatomic, assign) NSInteger timeCount;
+@property (nonatomic, strong) NSString *resultOfCalculate;
+@property (nonatomic) BOOL useInTheMiddleNumber;
 
 @end
 
@@ -33,7 +39,12 @@
 //    self.navigationItem.hidesBackButton = YES;
     [self checkSmallerWithSubaction];
     [self performCalculate];
-
+    self.timeCount = 25;
+    [self countDownTimer];
+    
+#warning TODO Perform Custom Back Home On Navigation
+// Code here
+    // Perform reset timer and set timeCount = 25;
     
 }
 
@@ -87,61 +98,96 @@
     if ([stringCalculate isEqualToString:@"*"]) {
         
         NSInteger multiplicationCalculate = numberFirst * numberSecond;
-        self.resultCalculate.text = [NSString stringWithFormat:@"%ld", (long)multiplicationCalculate];
+        self.resultOfCalculate = [NSString stringWithFormat:@"%ld", (long)multiplicationCalculate];
     }
     else if ([stringCalculate isEqualToString:@"+"])
     {
         NSInteger summationCalculate= numberFirst + numberSecond;
-        self.resultCalculate.text = [NSString stringWithFormat:@"%ld", (long)summationCalculate];
+        self.resultOfCalculate = [NSString stringWithFormat:@"%ld", (long)summationCalculate];
     }
     else if ([stringCalculate isEqualToString:@"-"])
     {
         NSInteger subactionCalculate = numberFirst - numberSecond;
-        self.resultCalculate.text = [NSString stringWithFormat:@"%ld", (long)subactionCalculate];
+        self.resultOfCalculate = [NSString stringWithFormat:@"%ld", (long)subactionCalculate];
     }
 }
 
-- (void)showAlertController : (NSString *)stringNumberCode
+/**
+ *  Perform count down timer
+ */
+- (void)countDownTimer
 {
-//    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"Number Code" message:stringNumberCode preferredStyle:UIAlertControllerStyleAlert];
-//    
-//    UIAlertAction *ok = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
-//        
-//    }];
-//    
-//    UIAlertAction *cancel = [UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel handler:nil];
-//    
-//    // Add Action to alert controller
-//    [alertController addAction:ok];
-//    [alertController addAction:cancel];
-//    
-//    [self presentViewController:alertController animated:YES completion:nil];
-    UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Number Code" message:stringNumberCode delegate:self cancelButtonTitle:@"Cancel" otherButtonTitles:@"OK", nil];
-    
-    [alertView show];
+    self.timer = [NSTimer scheduledTimerWithTimeInterval:0.5 target:self selector:@selector(updateTimer) userInfo:nil repeats:YES];
 }
 
-- (IBAction)buttonZero:(id)sender {
-    
-    [self showAlertController:[NSString stringWithFormat:@"%ld", (long)HDNumberZero]];
-    
+
+/**
+ *  Update time
+ */
+- (void)updateTimer
+{
+    if (self.timeCount == 0) {
+         NSLog(@"SAI ROI");
+        [self.timer invalidate];
+        self.timer = nil;
+    }
+    else
+    {
+        self.timeCount --;
+        self.labelTime.text = [NSString stringWithFormat:@"%ld", (long)self.timeCount];
+    }
 }
-- (IBAction)buttonOne:(id)sender {
+/**
+ *  Press number
+ *
+ *  @param sender button number
+ */
+- (IBAction)pressNumber:(UIButton *)sender {
     
-    [self showAlertController:[NSString stringWithFormat:@"%ld", (long)HDNumberOne]];
-}
-- (IBAction)buttonTwo:(id)sender {
+    NSInteger number = (sender).tag;
     
-    [self showAlertController:[NSString stringWithFormat:@"%ld", (long)HDNumberTwo]];
-}
-- (IBAction)buttonThree:(id)sender {
+    if (self.useInTheMiddleNumber) {
+        self.resultCalculate.text = [self.resultCalculate.text stringByAppendingString:[NSString stringWithFormat:@"%ld", number]];
+        
+        NSArray *arraySlipt = [self.resultCalculate.text componentsSeparatedByString:@"0"];
+        NSString *numberZero = arraySlipt[0];
+        if ([numberZero isEqualToString:@""]) {
+            
+            self.resultCalculate.text = arraySlipt[1];
+        }
+    }
     
-    [self showAlertController:[NSString stringWithFormat:@"%ld", (long)HDNUmberThree]];
-}
-- (IBAction)buttonFour:(id)sender {
+    else
+    {
+        self.resultCalculate.text = [NSString stringWithFormat:@"%ld",(long)number];
+        self.useInTheMiddleNumber = YES;
+    }
     
-    [self showAlertController:[NSString stringWithFormat:@"%ld", (long)HDNumberFour]];
+   
+    
+    if (self.timeCount != 0) {
+        
+        if ([self.resultCalculate.text isEqualToString:self.resultOfCalculate]) {
+            
+            NSLog(@"DUNG ROI");
+            [self.timer invalidate];
+        
+        }
+    }
+
 }
+/**
+ *  Reset number
+ *
+ *  @param sender sender of button
+ */
+- (IBAction)resetNumber:(id)sender {
+    
+    self.resultCalculate.text = @"0";
+    self.useInTheMiddleNumber = NO;
+}
+
+
 
 
 
--- Maths025s/Math025s/HDUtil.h
+++ Maths025s/Math025s/HDUtil.h
@@ -9,18 +9,4 @@
 #ifndef Math025s_HDUtil_h
 #define Math025s_HDUtil_h
 
-typedef NS_ENUM (NSInteger, HDNumber)
-{
-    HDNumberZero  = 0,
-    HDNumberOne   = 1,
-    HDNumberTwo   = 2,
-    HDNUmberThree = 3,
-    HDNumberFour  = 4,
-    HDNumberFive  = 5,
-    HDNumberSix   = 6,
-    HDNumberSeven = 7,
-    HDNumberEight = 8,
-    HDNumberNine  = 9,
-    HDNumberTen   = 10
-};
 #endif
